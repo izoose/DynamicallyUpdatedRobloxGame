b2d6a1e350d811943083c776ef4d065ba7d08dbe
zoose <124001738+izoose@users.noreply.github.com>
2025-11-16 20:15:04 -0500

    Change part anchoring to false in script.lua

--- script.lua
+++ script.lua
@@ -2,6 +2,6 @@ for i = 1, 100 do
     local p = Instance.new("Part")
     p.Size = Vector3.new(4, 4, 4)
     p.Position = Vector3.new(i * 5, 10, 0)
-    p.Anchored = true
+    p.Anchored = false
     p.Parent = workspace
 end
