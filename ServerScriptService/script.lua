6bf74eedfa450e336d632ab7f480813c3949a5bb
zoose <124001738+izoose@users.noreply.github.com>
2025-11-16 20:52:24 -0500

    Add script to generate 100 parts in Workspace
    This script generates 100 anchored parts in the Workspace.

---
+++ ServerScriptService/script.lua
@@ -0,0 +1,10 @@
+-- GitHub script example
+-- Generates 100 parts in Workspace
+
+for i = 1, 100 do
+    local p = Instance.new("Part")
+    p.Size = Vector3.new(4, 4, 4)
+    p.Position = Vector3.new(i * 5, 10, 0)
+    p.Anchored = true
+    p.Parent = workspace
+end
