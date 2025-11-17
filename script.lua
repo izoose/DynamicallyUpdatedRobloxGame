5fb4dd727dc3eee45c0e4db6222f185349c23fb4
zoose <124001738+izoose@users.noreply.github.com>
2025-11-16 20:16:45 -0500

    Anchor parts in script.lua

--- script.lua
+++ script.lua
@@ -2,6 +2,6 @@ for i = 1, 100 do
     local p = Instance.new("Part")
     p.Size = Vector3.new(4, 4, 4)
     p.Position = Vector3.new(i * 5, 10, 0)
-    p.Anchored = false
+    p.Anchored = true
     p.Parent = workspace
 end
